72d8a803e63bb0dfece6e41ff101867e22663951
Fazilmd152 <mohamedfazil152@gmail.com>
2024-11-25 09:35:27 +0530

    mongodb atlas integrated

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -7,7 +7,7 @@ config()
 async function addUsers(){
     try {
         await connectDatabase()
-        await User.deleteMany({ _id: { $nin: ['673e4b71fa97f9c2f270deab', '67421a2cdf9ee3954c5b4916'] } })
+        await User.deleteMany()
         console.log("deleted succesfully");
         await User.insertMany(seedUsers)
         console.log("users added succesfully");
